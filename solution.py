fcfd7d92bd264fd074b92c68787747b8b0eef719
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-14 09:44:55 +0900

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/no-idea/problem
+
+nm = input()
+arr = input().split()
+a = set(input().split())
+b = set(input().split())
+
+print(sum((i in a) - (i in b) for i in arr))
